6a8a3f4a2f31e2ca6c641d780abc4d9929bbc846
Sangdy <779730089@qq.com>
2016-10-04 16:53:29 +0800

    first commit

---
+++ client/src/components/ui/NewPost.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class NewPost extends Component {
+  getStyles() {
+    return {
+      content: {
+        width: '100%',
+        maxWidth: '600px',
+        margin: '30px auto',
+        backgroundColor: '#fff',
+        borderRadius: '10px',
+        boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
+      },
+      title: {
+        fontSize: '1.2em',
+        textAlign: 'center',
+        paddingTop: '20px'
+      }
+    };
+  }
+
+  render() {
+    const styles = this.getStyles();
+    return (
+      <div style={styles.content}>
+        <div style={styles.title}>写文章</div>
+      </div>
+    );
+  }
+}
+
+export default NewPost;
